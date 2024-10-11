62a44ac1e5e336db74b38f940fc17183630c87ad
DariusX1 <116304136+DariusX1@users.noreply.github.com>
2024-10-11 15:59:12 -0400

    Progress with the Database handling

---
+++ lib/database.dart
@@ -0,0 +1,65 @@
+import 'package:mongo_dart/mongo_dart.dart' as mongo;
+
+import 'package:mongo_dart/mongo_dart.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  Db? _db;
+
+  // Private constructor
+  DatabaseHelper._internal();
+
+  // Factory constructor to return the same instance
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+  // Method to initialize the database connection
+  Future<void> init() async {
+    if (_db == null) {
+      _db = await mongo.Db.create(
+          "mongodb+srv://USER:USER1@metronome-cluster.3otig.mongodb.net/metronome_db?retryWrites=true&w=majority");
+      await _db!.open();
+    }
+    if (_db != null && _db!.isConnected) {
+      print(
+          'Database connected. ${_db!.databaseName}'); // Correctly accessing the property
+    } else {
+      print('Database connection failed.');
+    }
+  }
+
+  // Method to get the database instance
+  Db get db {
+    if (_db == null) {
+      throw Exception('Database not initialized. Call init() first.');
+    }
+    return _db!;
+  }
+
+  Future<bool> insertStudent({
+    required String name,
+    required String pwd,
+  }) async {
+    if (_db == null) {
+      return false;
+    }
+
+    var studentsCollection = _db!.collection('Student');
+    var student = await studentsCollection.findOne(where.eq('name', name));
+
+    if (student != null) {
+      return false;
+    }
+
+    var newStudent = {
+      'name': name,
+      'pwd': pwd,
+      'assigned_music': [],
+      'subgroup_id': null
+    };
+
+    await studentsCollection.insert(newStudent);
+    return true;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:mongo_dart/mongo_dart.dart' as mongo;
+import 'database.dart';
 
 void main() async {
-  var db = await mongo.Db.create("mongodb+srv://USER:USER1@metronome-cluster.3otig.mongodb.net/metronome_db?retryWrites=true&w=majority");
-  await db.open();
-  print('Connected to database: ${db.databaseName}');
+  var db = DatabaseHelper();
+  await db.init();
+
+  bool result = await db.insertStudent(name: 'Adnan', pwd: '2782738');
+  print(result);
+
   runApp(MetronomeApp());
 }
 
@@ -20,7 +23,6 @@ class _MetronomeAppState extends State<MetronomeApp> {
   final player = AudioPlayer();
   bool playing = true;
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,109 +32,90 @@ class _MetronomeAppState extends State<MetronomeApp> {
           centerTitle: true,
           backgroundColor: Colors.red[400],
         ),
-        drawer:  Drawer(
-          child: ListView(
-            padding: const EdgeInsets.all(0),
-            children: <Widget>[
-              ListTile(
-                title: const Text('Hello there'),
-                onTap: () {},
-              )
-            ],
-          )
-        ),
+        drawer: Drawer(
+            child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            ListTile(
+              title: const Text('Hello there'),
+              onTap: () {},
+            )
+          ],
+        )),
         body: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('BPM: ${_bpm.toInt()}'),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
 
               //adding a slider for the BPM
-            //min: 40, max: 200
-            children: [
-
-              ElevatedButton(
-                onPressed: () {
-                  setState(()
-                  {
-                    if(_bpm > 40)
-                      {
+              //min: 40, max: 200
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_bpm > 40) {
                         _bpm = _bpm - 1;
                       }
-                  });
-                },
-                child: Text("-"),
-              ),
-             Slider(
-                              min: 40,
-                              max: 200,
-                              value: _bpm,
-                              //If changed, new value is newBPM
-                              onChanged: (newBPM) {
-
-                                setState(()
-                                {
-                                //setting the BPM to new BPM
-                                  _bpm = newBPM;
-                                });},
-
+                    });
+                  },
+                  child: Text("-"),
                 ),
-
-              ElevatedButton(
-                onPressed: ()
-                {
-                  setState(()
-                  {
-                    if(_bpm < 199)
-                    {
-                      _bpm = _bpm + 1;
-                    }
-                  });
-                },
-                child: Text("+"),
-              ),
-
-
-            ],
-            )
-
-
-
-            ,Row(
+                Slider(
+                  min: 40,
+                  max: 200,
+                  value: _bpm,
+                  //If changed, new value is newBPM
+                  onChanged: (newBPM) {
+                    setState(() {
+                      //setting the BPM to new BPM
+                      _bpm = newBPM;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_bpm < 199) {
+                        _bpm = _bpm + 1;
+                      }
+                    });
+                  },
+                  child: Text("+"),
+                ),
+              ],
+            ),
+            Row(
               //centering an axis along the center of the screen
               mainAxisAlignment: MainAxisAlignment.center,
               //underneath the axis, add buttons(children)
               children: [
                 ElevatedButton(
                   //when pressed, start the metronome
-                  onPressed: ()
-                  async {
-                  playing = true;
+                  onPressed: () async {
+                    playing = true;
 
                     //looping the sound
-                    while (playing)
-                      {
-                        //calculation desired duration of one beat in seconds
-                        double oneBeat = 60/_bpm;
+                    while (playing) {
+                      //calculation desired duration of one beat in seconds
+                      double oneBeat = 60 / _bpm;
 
-                        //length of tick_sound is 0.022. length of one beat - length of tick sound is wait time
-                        double waitTime = oneBeat-0.156;
-                        //setting source of the audio file
-                        await player.setSource(AssetSource('tick_sound_156.wav'));
-                        //playing the audio file
-                        await player.resume();
-                        //delaying the loop (length of tick sound)
-                        await Future.delayed(Duration(milliseconds: 156));
-                        //delaying the loop(length of wait time
-                        await Future.delayed(Duration(milliseconds: (waitTime * 1000).toInt()));
-                        //Now, one full beat has been completed. The loop will loop again!
+                      //length of tick_sound is 0.022. length of one beat - length of tick sound is wait time
+                      double waitTime = oneBeat - 0.156;
+                      //setting source of the audio file
+                      await player.setSource(AssetSource('tick_sound_156.wav'));
 
-                      }
-
-
+                      await player.setVolume(10000000);
+                      //playing the audio file
+                      await player.resume();
+                      //delaying the loop (length of tick sound)
+                      await Future.delayed(Duration(milliseconds: 156));
+                      //delaying the loop(length of wait time
+                      await Future.delayed(
+                          Duration(milliseconds: (waitTime * 1000).toInt()));
+                      //Now, one full beat has been completed. The loop will loop again!
+                    }
                   },
                   child: Text('Start'),
                 ),
@@ -147,57 +130,43 @@ class _MetronomeAppState extends State<MetronomeApp> {
                 ),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Beat 1'),
-                ),
-                ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Beat 2'),
-                ),
-                ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Beat 3'),
-                ),
-                ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Beat 4'),
-                )
-
-              ]
-            ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text('♩', style: new TextStyle(fontSize: 50.0)),
-                  ),
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text('♪', style: TextStyle(fontSize: 50.0)),
-                  ),
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text('♫', style: TextStyle(fontSize: 50.0)),
-                  ),
-                  ElevatedButton(
-                    onPressed: (){},
-                    child: Text('♬', style: TextStyle(fontSize: 50.0)),
-                  )
-
-                ]
-            )
-
-
-
-
-
-
-        ],
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Beat 1'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Beat 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Beat 3'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Beat 4'),
+              )
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('♩', style: new TextStyle(fontSize: 50.0)),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('♪', style: TextStyle(fontSize: 50.0)),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('♫', style: TextStyle(fontSize: 50.0)),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('♬', style: TextStyle(fontSize: 50.0)),
+              )
+            ])
+          ],
         ),
       ),
     );
