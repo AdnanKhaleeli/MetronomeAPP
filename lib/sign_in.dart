2b7319a3c39a991b3f65c0fe84974f408a792c6f
DariusX1 <116304136+DariusX1@users.noreply.github.com>
2024-10-23 00:16:34 -0400

    Added Login and Signin Buttons

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -35,6 +35,7 @@ class _LogInState extends State<LogIn> {
   final _formKey = GlobalKey<FormState>();
   String? _username;
   String? _password;
+  bool sign_up = false;
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +46,11 @@ class _LogInState extends State<LogIn> {
             margin: EdgeInsets.all(40),
             child: TextFormField(
                 decoration: const InputDecoration(
-                  labelText: 'Username',
-                  hintText: 'Enter your username',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)
-                ),
-               
+                    labelText: 'Username',
+                    hintText: 'Enter your username',
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter a username';
@@ -58,16 +58,15 @@ class _LogInState extends State<LogIn> {
                   return null;
                 }),
           ),
-            Container(
+          Container(
             margin: EdgeInsets.all(40),
             child: TextFormField(
                 decoration: const InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)
-                ),
-               
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    border: OutlineInputBorder(),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter a password';
@@ -75,24 +74,42 @@ class _LogInState extends State<LogIn> {
                   return null;
                 }),
           ),
-           Container(
-            margin: EdgeInsets.all(40),
-            child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Profile name',
-                  hintText: 'Enter your profile name',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0)
-                ),
-               
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a profile name';
-                  }
-                  return null;
-                }),
-
-           ),
+          if (sign_up)
+            Container(
+              margin: EdgeInsets.all(40),
+              child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Profile name',
+                      hintText: 'Enter your profile name',
+                      border: OutlineInputBorder(),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 15.0)),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a profile name';
+                    }
+                    return null;
+                  }),
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                     sign_up = false; // Update state to show login fields
+                  });
+                  },
+                  child: Text('Login')),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                    sign_up = true; // Update state to show login fields
+                    });
+                  },
+                  child: Text('SignUp')),
+            ],
+          )
         ]));
   }
 }
