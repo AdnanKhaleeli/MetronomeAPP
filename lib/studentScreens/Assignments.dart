60db649f9723496feb1d7d7cc7fd627aa8e5a9dd
DariusX1 <116304136+DariusX1@users.noreply.github.com>
2024-11-13 21:04:59 -0500

    Fixed colors

--- lib/studentScreens/Assignments.dart
+++ lib/studentScreens/Assignments.dart
@@ -16,14 +16,13 @@ class Assignments extends StatefulWidget {
 
 class _AssignmentsState extends State<Assignments> {
   Map<String, List<int?>> currentBpmMap = {};
-  Map<String, double> averageBpmMap = {}; // Map to store average BPM for each piece
 
-  // Fetch the current BPM and calculate the average BPM for each piece
   Future<void> fetchCurrentBPM(Piece piece) async {
     List<int?> currentBpmList = [];
 
     for (int sectionIndex = 0; sectionIndex < piece.sections.length; sectionIndex++) {
       final section = piece.sections[sectionIndex];
+
       int currentBpm = await DatabaseHelper().getCurrentUserBPMForSection(
         sectionIndex,
         widget.student!.userId,
@@ -33,28 +32,9 @@ class _AssignmentsState extends State<Assignments> {
       currentBpmList.add(currentBpm);
     }
 
-    // Calculate the average BPM for this piece
-    double averageBpm = _calculateAverageBpm(currentBpmList);
-
-    // Update state once all BPMs are fetched
     setState(() {
       currentBpmMap[piece.pieceId] = currentBpmList;
-      averageBpmMap[piece.pieceId] = averageBpm;
     });
-  }
-
-  // Method to calculate average BPM, excluding "N/A" (null values)
-  double _calculateAverageBpm(List<int?> bpms) {
-    int sum = 0;
-    int count = 0;
-
-    for (var bpm in bpms) {
-      if (bpm != null && bpm != -1) { // Exclude null and N/A (-1) values
-        sum += bpm;
-        count++;
-      }
-    }
-    return count > 0 ? sum / count : 0; // Avoid division by zero
   }
 
   @override
@@ -92,123 +72,149 @@ class _AssignmentsState extends State<Assignments> {
               child: widget.pieces.isEmpty
                   ? Center(child: CircularProgressIndicator())
                   : ListView.builder(
-                itemCount: widget.pieces.length,
-                itemBuilder: (context, index) {
-                  final piece = widget.pieces[index];
-                  final sections = piece.sections;
-                  final pieceCurrentBpm = currentBpmMap[piece.pieceId];
-                  final averageBpm = averageBpmMap[piece.pieceId];
+                      itemCount: widget.pieces.length,
+                      itemBuilder: (context, index) {
+                        final piece = widget.pieces[index];
+                        final sections = piece.sections;
+                        final pieceCurrentBpm = currentBpmMap[piece.pieceId];
 
-                  return Card(
-                    margin: EdgeInsets.all(8.0),
-                    elevation: 6,
-                    color: Color(0xFF1C1C1C),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            piece.pieceName,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Average Tempo: ${averageBpm?.toStringAsFixed(2) ?? "N/A"} BPM',
-                            style: TextStyle(
-                              color: Colors.grey[400],
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          LayoutBuilder(
-                            builder: (context, constraints) {
-                              return Center(
-                                child: ConstrainedBox(
-                                  constraints: BoxConstraints(
-                                    maxWidth: constraints.maxWidth * 0.9,
-                                  ),
-                                  child: DataTable(
-                                    columnSpacing: 10,
-                                    columns: [
-                                      DataColumn(
-                                        label: Text(
-                                          'Section',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                      DataColumn(
-                                        label: Text(
-                                          'Goal BPM',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                      DataColumn(
-                                        label: Text(
-                                          'Current BPM',
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                    rows: sections.map<DataRow>((section) {
-                                      final currentBpm = pieceCurrentBpm != null
-                                          ? pieceCurrentBpm[sections.indexOf(section)]
-                                          : -1;
-                                      final currentBpmText = currentBpm == -1 ? "N/A" : currentBpm.toString();
-
-                                      Color? backgroundColorForCurrentBpm;
-                                      if (currentBpm == -1 || currentBpm! >= section.goalBpm) {
-                                        backgroundColorForCurrentBpm = Colors.green.shade700;
-                                      } else if (currentBpm! < section.goalBpm) {
-                                        backgroundColorForCurrentBpm = Colors.red.shade700;
-                                      }
-
-                                      return DataRow(cells: [
-                                        DataCell(Text(
-                                          section.sectionName,
-                                          style: TextStyle(
-                                            color: Colors.grey[400],
-                                          ),
-                                        )),
-                                        DataCell(Text(
-                                          '${section.goalBpm}',
-                                          style: TextStyle(
-                                            color: Colors.grey[400],
-                                          ),
-                                        )),
-                                        DataCell(Container(
-                                          color: backgroundColorForCurrentBpm,
-                                          child: Text(
-                                            currentBpmText,
-                                            style: TextStyle(
-                                              color: Colors.grey[400],
-                                            ),
-                                          ),
-                                        )),
-                                      ]);
-                                    }).toList(),
+                        return Card(
+                          margin: EdgeInsets.all(8.0),
+                          elevation: 6,
+                          color: Color(0xFF1C1C1C),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  piece.pieceName,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
                                   ),
                                 ),
-                              );
-                            },
+                                SizedBox(height: 8),
+                                LayoutBuilder(
+                                  builder: (context, constraints) {
+                                    return Center(
+                                      child: ConstrainedBox(
+                                        constraints: BoxConstraints(
+                                          maxWidth: constraints.maxWidth * 0.9,
+                                        ),
+                                        child: DataTable(
+                                          columnSpacing: 10,
+                                          columns: [
+                                            DataColumn(
+                                              label: Align(
+                                                alignment: Alignment.center,
+                                                child: Text(
+                                                  'Section',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            DataColumn(
+                                              label: Align(
+                                                alignment: Alignment.center,
+                                                child: Text(
+                                                  'Goal BPM',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            DataColumn(
+                                              label: Align(
+                                                alignment: Alignment.center,
+                                                child: Text(
+                                                  'Current BPM',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                          rows: sections.map<DataRow>((section) {
+                                            final currentBpm = pieceCurrentBpm != null
+                                                ? pieceCurrentBpm[sections.indexOf(section)]
+                                                : -1;
+                                            final currentBpmText = currentBpm == -1 ? "N/A" : currentBpm.toString();
+
+                                            Color? backgroundColorForCurrentBpm;
+                                            if (currentBpm == -1 || currentBpm! >= section.goalBpm) {
+                                              backgroundColorForCurrentBpm = Colors.green.shade700;
+                                            } else if (currentBpm! < section.goalBpm) {
+                                              if (section.goalBpm - currentBpm <= 10) {
+                                                backgroundColorForCurrentBpm = Colors.yellow.shade800;
+                                              } else {
+                                                backgroundColorForCurrentBpm = Colors.red.shade700;
+                                              }
+                                            }
+
+                                            return DataRow(cells: [
+                                              DataCell(
+                                                Align(
+                                                  alignment: Alignment.center,
+                                                  child: Text(
+                                                    section.sectionName,
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              DataCell(
+                                                Align(
+                                                  alignment: Alignment.center,
+                                                  child: Text(
+                                                    '${section.goalBpm}',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                              DataCell(
+                                                Container(
+                                                  color: backgroundColorForCurrentBpm,
+                                                  child: Align(
+                                                    alignment: Alignment.center,
+                                                    child: Padding(
+                                                      padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 8.0),
+                                                      child: Text(
+                                                        currentBpmText,
+                                                        style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ]);
+                                          }).toList(),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             ),
           ],
         ),
